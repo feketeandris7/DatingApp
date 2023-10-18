bf002e755b3c990b4e4688dc95ca6328a20a0121
Fekete András <fekete.andris7@gmail.com>
2023-10-18 21:50:23 +0200

    Basics for the backend API

--- DatingApp/API/Program.cs
+++ DatingApp/API/Program.cs
@@ -1,11 +1,18 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<DataContext>(opt =>
+{
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(); 
 
 var app = builder.Build();
 
@@ -14,6 +21,11 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+}
+
+using (var context = new DataContext())
+{
+    context.Database.EnsureCreated();
 }
 
 app.UseHttpsRedirection();
