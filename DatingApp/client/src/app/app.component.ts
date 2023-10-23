92dff9144e289253b8da2a1ec145ba524c68e0eb
Fekete András <fekete.andris7@gmail.com>
2023-10-24 00:07:35 +0200

    dropdowntoggle + loginservice

--- DatingApp/client/src/app/app.component.ts
+++ DatingApp/client/src/app/app.component.ts
@@ -14,8 +14,8 @@ export class AppComponent implements OnInit {
 
   ngOnInit(): void {
     this.http.get('https://localhost:44303/api/Users').subscribe({
-      next: response => this.users = response,
-      error: error => console.log(error),
+      next: (response) => (this.users = response),
+      error: (error) => console.log(error),
       complete: () => console.log('Request completed!')
     });
   }
--- DatingApp/client/src/app/app.module.ts
+++ DatingApp/client/src/app/app.module.ts
@@ -6,19 +6,21 @@ import { AppComponent } from './app.component';
 
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavComponent } from './nav/nav.component';
+import { FormsModule } from '@angular/forms';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, NavComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule,
+    BsDropdownModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
- 
+export class AppModule {}
