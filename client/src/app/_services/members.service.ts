a35831ed4180cb3fab134b6bb9bd54b93623b1ef
Fekete András <94868606+feketeandris7@users.noreply.github.com>
2023-11-21 10:09:13 +0100

    Implementing paging

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -1,8 +1,10 @@
-import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { HttpClient, HttpHeaders, HttpParams } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { environment } from "src/environments/environment";
 import { Member } from "../_models/member";
 import { map, of } from "rxjs";
+import { PaginatedResult } from "../_models/pagination";
+import { UserParams } from "../_models/userParams";
 
 @Injectable({
   providedIn: "root"
@@ -13,14 +15,42 @@ export class MembersService {
 
   constructor(private http: HttpClient) {}
 
-  getMembers() {
-    if (this.members.length > 0) return of(this.members);
-    return this.http.get<Member[]>(this.baseUrl + "Users").pipe(
-      map((members) => {
-        this.members = members;
-        return members;
+  getMembers(userParams: UserParams) {
+    let params = this.getPaginationHeaders(
+      userParams.pageNumber,
+      userParams.pageSize
+    );
+
+    params = params.append("minAge", userParams.minAge);
+    params = params.append("maxAge", userParams.maxAge);
+    params = params.append("gender", userParams.gender);
+
+    return this.getPaginatedResult<Member[]>(this.baseUrl + "Users", params);
+  }
+
+  private getPaginatedResult<T>(url: string, params: HttpParams) {
+    const paginatedResult: PaginatedResult<T> = new PaginatedResult<T>();
+    return this.http.get<T>(url, { observe: "response", params }).pipe(
+      map((response) => {
+        if (response.body) {
+          paginatedResult.result = response.body;
+        }
+        const pagination = response.headers.get("Pagination");
+        if (pagination) {
+          paginatedResult.pagination = JSON.parse(pagination);
+        }
+        return paginatedResult;
       })
     );
+  }
+
+  private getPaginationHeaders(pageNumber: number, pageSize: number) {
+    let params = new HttpParams();
+
+    params = params.append("pageNumber", pageNumber);
+    params = params.append("pageSize", pageSize);
+
+    return params;
   }
 
   getMember(username: string) {
@@ -39,10 +69,10 @@ export class MembersService {
   }
 
   setMainPhoto(photoId: number) {
-    return this.http.put(this.baseUrl + 'Users/set-main-photo/' + photoId, {})
+    return this.http.put(this.baseUrl + "Users/set-main-photo/" + photoId, {});
   }
 
   deletePhoto(photoId: number) {
-    return this.http.delete(this.baseUrl + 'Users/delete-photo/' + photoId);
+    return this.http.delete(this.baseUrl + "Users/delete-photo/" + photoId);
   }
 }
